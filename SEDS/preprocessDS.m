318d4fd63e6ff9601db10f473a590030022ac4a9
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-11-01 09:45:33 +0100

    remove a line that would subtract the last point to the segment

--- SEDS/preprocessDS.m
+++ SEDS/preprocessDS.m
@@ -116,9 +116,6 @@ for i=1:length(demos)
     %saving the final point (target) of each demo
     xT(:,i) = demos{i}(:,end); 
     
-    % shifting demos to the origin
-    tmp = tmp - repmat(xT(:,i),1,size(tmp,2));
-    
     % I ADDED THIS!
     tmp = -1*tmp;
     tmp_d = -1*tmp_d;
