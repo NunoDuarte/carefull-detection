e73f3ca821c70bf6b75cce925b1bed7ab344b70a
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-11-10 17:27:17 +0100

    update variables

--- DataProcessing/extractGiving.m
+++ DataProcessing/extractGiving.m
@@ -4,20 +4,20 @@ Wgiving = W{3}(518:750,:);
 
 
 figure()
-plot3(Wl(:,1), Wl(:,2), Wl(:,3), '.');
+plot3(Wgiving(:,1), Wgiving(:,2), Wgiving(:,3), '.');
 
 %% Segment data of elbow
 
-El = E{3}(518:750,:);
+Egiving = E{3}(518:750,:);
 
 
 figure()
-plot3(El(:,1), El(:,2), El(:,3), '.');
+plot3(Egiving(:,1), Egiving(:,2), Egiving(:,3), '.');
 
 %% Segment data of Shoulder
 
-Sl = S{3}(518:750,:);
+Sgiving = S{3}(518:750,:);
 
 figure();
-plot3(Sl(:,1), Sl(:,2), Sl(:,3), '.');
+plot3(Sgiving(:,1), Sgiving(:,2), Sgiving(:,3), '.');
 
