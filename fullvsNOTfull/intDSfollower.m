4e60916b2aa3a7335c37ae1520e8ec98daf53a75
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-12-09 12:33:26 +0100

    streamlines 3d in the correct dimensions

--- fullvsNOTfull/intDSfollower.m
+++ fullvsNOTfull/intDSfollower.m
@@ -139,19 +139,6 @@ end
 
 %%
 % plotting the result
-figure('name','Results from Simulation','position',[265   200   520   720])
-sp(1)=subplot(3,1,1);
-hold on; box on
-plot3(Data(1,:),Data(2,:),Data(3,:),'r.')
- 
-axis(sp(1),'tight')
-ax=get(sp(1));
-axis(sp(1),...
-    [ax.XLim(1)-(ax.XLim(2)-ax.XLim(1))/10 ax.XLim(2)+(ax.XLim(2)-ax.XLim(1))/10 ...
-    ax.YLim(1)-(ax.YLim(2)-ax.YLim(1))/10 ax.YLim(2)+(ax.YLim(2)-ax.YLim(1))/10]);
-plot(sp(1),0,0,'k*','markersize',15,'linewidth',3)
-D = axis(sp(1));
-
 % 
 % % plotting streamlines
 % figure('name','Streamlines','position',[800   90   560   320])
@@ -167,6 +154,7 @@ D = axis(sp(1));
 figure('name','Streamlines','position',[800   90   560   320])
 % streamlines
 quality='low';
+plot3(Data(1,:), Data(2,:), Data(3,:), 'r.')
 
 if strcmpi(quality,'high')
     nx=600;
@@ -180,14 +168,16 @@ else
     nz = ny;
 end
 
-ax.XLim = D(1:2);
-ax.YLim = D(3:4);
+hold on
+
+ax.XLim = xlim;
+ax.YLim = ylim;
+ax.ZLim = zlim;
 ax_x=linspace(ax.XLim(1),ax.XLim(2),nx); %computing the mesh points along each axis
 ax_y=linspace(ax.YLim(1),ax.YLim(2),ny); %computing the mesh points along each axis
-ax_z=linspace(ax.YLim(1),ax.YLim(2),ny); %computing the mesh points along each axis
+ax_z=linspace(ax.ZLim(1),ax.ZLim(2),nz); %computing the mesh points along each axis
 [x_tmp, y_tmp, z_tmp]=meshgrid(ax_x,ax_y, ax_z); %meshing the input domain
 x=[x_tmp(:) y_tmp(:) z_tmp(:)]';
-z=zeros(1,nx*ny);
 
 %GMR(Priors,Mu,Sigma,x,1:d,d+1:2*d);
 xd = GMR(Priors,Mu,Sigma,x,1:d,d+1:2*d); %compute outputs
@@ -196,7 +186,5 @@ x=[x_tmp(:) y_tmp(:) z_tmp(:)];
 quiver3(x(:,1),x(:,2),x(:,3),xd(1,:)',xd(2,:)',xd(3,:)',3,'color','blue');
 %streamslice(x_tmp,y_tmp, reshape(xd(1,:),ny,ny,nz),reshape(xd(2,:),ny,nx,nz),reshape(xd(3,:),ny,nx,nz),1,'method','cubic')
 
-hold on
-plot3(Data(1,:),Data(2,:), Data(3,:), 'r.')
 end
 
