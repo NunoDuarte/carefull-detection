4f5d9250e046d02e614f2aa31a9481e056cdab61
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-12-10 20:23:58 +0100

    apply task adaptation belief system - mahdi work

---
+++ fullvsNOTfull/beliefDS.m
@@ -0,0 +1,46 @@
+%% Load Path
+addpath('../SEDS')
+addpath('data')
+addpath('../../Khansari/SEDS/SEDS_lib')
+addpath('../../Khansari/SEDS/GMR_lib')
+
+% Which Person to choose (Salman, Leo, Bernardo)
+[E, F] = read('All');
+
+%% Belief System for 2 DS
+
+b1 = 0.5;
+b2 = 0.5;
+b = [b1, b2];
+
+% pick one trajectory
+testX = E{2}; 
+
+% remove nonzeros
+testXn(:,1) = nonzeros(testX(:,2));
+testXn(:,2) = nonzeros(testX(:,3));
+testXn(:,3) = nonzeros(testX(:,4));
+test3{1}(1,:) = testXn(:,1)';
+test3{1}(2,:) = testXn(:,2)';
+test3{1}(3,:) = testXn(:,3)'; 
+
+plotting = 0;    % do you want to plot the 3D versions?
+[test3D, test2Dorigin, test2D] = processData(test3, plotting);
+
+%% Load DS parameters
+
+MuE = load('MuE.mat');
+MuE = MuE.Mu;
+PriorsE = load('PriorsE.mat');
+PriorsE = PriorsE.Priors;
+SigmaE = load('SigmaE.mat');
+SigmaE = SigmaE.Sigma;
+
+MuF = load('MuF.mat');
+MuF = MuF.Mu;
+PriorsF = load('PriorsF.mat');
+PriorsF = PriorsF.Priors;
+SigmaF = load('SigmaF.mat');
+SigmaF = SigmaF.Sigma;
+
+%% 
