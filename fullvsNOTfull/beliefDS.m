04a1f9f3c3b5a7fe07a6826f42b58a3e1e684b15
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-12-11 15:51:48 +0100

    belief system - mahdi approach - implemented

--- fullvsNOTfull/beliefDS.m
+++ fullvsNOTfull/beliefDS.m
@@ -1,4 +1,7 @@
 %% Load Path
+clear all
+clc
+
 addpath('../SEDS')
 addpath('data')
 addpath('../../Khansari/SEDS/SEDS_lib')
@@ -9,12 +12,8 @@ addpath('../../Khansari/SEDS/GMR_lib')
 
 %% Belief System for 2 DS
 
-b1 = 0.5;
-b2 = 0.5;
-b = [b1, b2];
-
 % pick one trajectory
-testX = E{2}; 
+testX = F{3}; 
 
 % remove nonzeros
 testXn(:,1) = nonzeros(testX(:,2));
@@ -24,12 +23,12 @@ test3{1}(1,:) = testXn(:,1)';
 test3{1}(2,:) = testXn(:,2)';
 test3{1}(3,:) = testXn(:,3)'; 
 
-plotting = 0;    % do you want to plot the 3D versions?
-[test3D, test2Dorigin, test2D] = processData(test3, plotting);
+% plotting = 0;    % do you want to plot the 3D versions?
+% [test3D, test2Dorigin, test2D] = processData(test3, plotting);
 
-%% Origin the Data
+%% Center the Data in the Origin
 
-testXn = test3D{1};
+testXn = test3{1};
 testXn = testXn - testXn(:,end);
 
 %% Load DS parameters
@@ -61,20 +60,47 @@ dt = 0.1;
 testX_d = diff(testXn,1,2)/dt;
 
 %% Run each DS to get the desired velocity?
-opt_sim.dt = 0.05;
-opt_sim.i_max = 3000;
+opt_sim.dt = 0.01;
+opt_sim.i_max = 1;
 opt_sim.tol = 0.001;
+opt_sim.plot = 0;
+
+b1 = 0.5;
+b2 = 0.5;
+b = [b1, b2];
+b1_d = 0;
+b2_d = 0;
+b_d = [b1_d, b2_d];
+epsilon = 200; % adaptation rate
 
 d = length(PriorsE); %dimension of data
 xT = [0; 0; 0];
-for i = 1:1
-    
-    for j = 1:1
+Xd = [];
+
+B = [];
+for j = 1:length(testXn)-1   
+    for i = 1:2
         
         x0 = abs(testXn(:,j));
+        
         fn_handle = @(x) GMR(Priors{i},Mu{i},Sigma{i},x,1:d,d+1:2*d);
         [x, xd, tmp, xT]=Simulation(x0,xT,fn_handle,opt_sim); %running the simulator
+
+        % error (real velocity - desired velocity)
+        ed = testX_d(:,j) - xd(:,1);
+        
+        Xd = [Xd; xd(:,1)'];
+        
+        b_d(i) = epsilon * (ed'*xd(:,1) + (b(i) - 0.5)*norm(xd(:,1), 2)); 
+        
+        B_d = winnertakeall(b, b_d);
+        
+        b(i) = b(i) + B_d(i)*0.1;
+        b(i) = max(0., min(1., b(i)));
+        B = [B; b];
+
     end
+    b(2) = 1. - b(1);
 end
 
 
