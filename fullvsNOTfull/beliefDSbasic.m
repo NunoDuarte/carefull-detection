9815139752bd3e466dc2c911c1dc710c9cd03eee
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-12-31 17:32:01 +0100

    fix two bugs in the output of B

--- fullvsNOTfull/beliefDSbasic.m
+++ fullvsNOTfull/beliefDSbasic.m
@@ -1,8 +1,49 @@
 % xdot = A*x - slow
 % xdot = 2A*x - fast
 
-testXn = 
+t = 40:-1:0;
+testXn = (t.^2)*0.1;
 
 %% Real Velocity of testX
 dt = 0.02;
 testX_d = diff(testXn,1,2)/dt;
+
+A = [1, 2];
+%% Belief DS
+
+b1 = 0.5;
+b2 = 0.5;
+b = [b1, b2];
+b1_d = 0;
+b2_d = 0;
+b_d = [b1_d, b2_d];
+epsilon = 5; % adaptation rate
+
+Xd = [];
+B = [];
+B = [B; b];
+for j = 1:length(testXn)-1   
+    for i = 1:2
+        
+        x0 = abs(testXn(:,j));
+        
+        xd = A(i)*x0; %running the simulator
+
+        % error (real velocity - desired velocity)
+        ed = testX_d(:,j) - xd(:,1);
+        
+        Xd = [Xd; xd(:,1)'];
+        
+        b_d(i) = epsilon * (ed'*xd(:,1) + (b(i) - 0.5)*norm(xd(:,1), 2)); 
+    end
+    
+    B_d = winnertakeall(b, b_d);
+    
+    for i = 1:2
+        b(i) = b(i) + B_d(i)*0.1;
+        b(i) = max(0., min(1., b(i)));
+    end
+    b(2) = 1. - b(1);
+    B = [B; b];
+    
+end
