541c47d0a1da3ca1a325db17fc42ab076055694c
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-01-30 17:26:20 +0100

    last filtering to remove some bad segmentation

--- fullvsNOTfull/read.m
+++ fullvsNOTfull/read.m
@@ -11,40 +11,114 @@ function [E, F] = read(name, object)
         F{2} = csvread('data/Salman/full/1_right.csv');
         F{3} = csvread('data/Salman/full/2_right.csv');
         F{4} = csvread('data/Salman/full/3_right.csv');
+
+    % reading data of Kunpeng
+    elseif strcmp(name,'Kunpeng')
+        if strcmp(object,'plastic-cup')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{1}(135:end,:) = 0;
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{2}(130:end,:) = 0;
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            E{3}(150:end,:) = 0;
+            E{4} = csvread(['data/' name '/' object '/empty/3_right.csv']);
+            E{4}(100:end,:) = 0;
+            E{5} = csvread(['data/' name '/' object '/empty/4_right.csv']);
+            E{5}(120:end,:) = 0;
+            %
+            F{1} = csvread(['data/' name '/' object '/full/0_right.csv']);
+            F{1}(100:end,:) = 0;
+            F{2} = csvread(['data/' name '/' object '/full/1_right.csv']);
+            F{2}(200:end,:) = 0;
+            F{3} = csvread(['data/' name '/' object '/full/4_right.csv']);
+            F{3}(200:end,:) = 0;
+
+        elseif strcmp(object, 'red-cup')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{1}(85:end,:) = 0;
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            %
+            F{1} = csvread(['data/' name '/' object '/full/1_right.csv']);
+            F{1}(130:end,:) = 0;
+            F{2} = csvread(['data/' name '/' object '/full/2_right.csv']);
+            F{2}(150:end,:) = 0;
+            F{3} = csvread(['data/' name '/' object '/full/3_right.csv']);
+        elseif strcmp(object,'wine-glass')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            E{4} = csvread(['data/' name '/' object '/empty/3_right.csv']);
+            E{5} = csvread(['data/' name '/' object '/empty/4_right.csv']);
+            %
+            F{1} = csvread(['data/' name '/' object '/full/0_right.csv']);
+            F{2} = csvread(['data/' name '/' object '/full/1_right.csv']);
+            F{3} = csvread(['data/' name '/' object '/full/2_right.csv']);
+            F{4} = csvread(['data/' name '/' object '/full/3_right.csv']);
+            F{5} = csvread(['data/' name '/' object '/full/4_right.csv']);
+        end     
         
     % reading data of Leo
     elseif strcmp(name,'Leo')
         if strcmp(object, 'red-cup')
-            % % Empty
-            E{1} = csvread('data/Leo/red-cup/empty/0_right.csv');
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
             E{1}(1100:end,:) = 0;
-            E{2} = csvread('data/Leo/red-cup/empty/1_right.csv');
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
             E{2}(1300:end,:) = 0;
-            E{3} = csvread('data/Leo/red-cup/empty/2_right.csv');
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
             %
-            % Full
-            F{1} = csvread('data/Leo/red-cup/full/1_right.csv');
+            F{1} = csvread(['data/' name '/' object '/full/1_right.csv']);
             F{1}(1300:end,:) = 0;
-            F{2} = csvread('data/Leo/red-cup/full/2_right.csv');
-            F{3} = csvread('data/Leo/red-cup/full/3_right.csv');
+            F{2} = csvread(['data/' name '/' object '/full/2_right.csv']);
+            F{3} = csvread(['data/' name '/' object '/full/3_right.csv']);
             F{3}(2500:end,:) = 0;
+        elseif strcmp(object,'plastic-cup')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{1}(650:end,:) = 0;
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            E{3}(1100:end,:) = 0;
+            %
+            F{1} = csvread(['data/' name '/' object '/full/1_right.csv']);
+            F{2} = csvread(['data/' name '/' object '/full/2_right.csv']);
+        elseif strcmp(object,'red-mug')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            E{4} = csvread(['data/' name '/' object '/empty/3_right.csv']);
+            E{5} = csvread(['data/' name '/' object '/empty/4_right.csv']);
+            E{6} = csvread(['data/' name '/' object '/empty/5_right.csv']);
+            %
+            F{1} = csvread(['data/' name '/' object '/half/0_right.csv']);
+            F{2} = csvread(['data/' name '/' object '/half/1_right.csv']);
+            F{3} = csvread(['data/' name '/' object '/half/2_right.csv']);
+            F{4} = csvread(['data/' name '/' object '/half/3_right.csv']);
+            F{5} = csvread(['data/' name '/' object '/half/4_right.csv']);
+        elseif strcmp(object,'champagne')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            E{4} = csvread(['data/' name '/' object '/empty/3_right.csv']);
+            E{5} = csvread(['data/' name '/' object '/empty/4_right.csv']);
+            %
+            F{1} = csvread(['data/' name '/' object '/full/0_right.csv']);
+            F{2} = csvread(['data/' name '/' object '/full/1_right.csv']);
+            F{3} = csvread(['data/' name '/' object '/full/2_right.csv']);
+            F{4} = csvread(['data/' name '/' object '/full/3_right.csv']);
+            F{5} = csvread(['data/' name '/' object '/full/4_right.csv']);
+        elseif strcmp(object,'wine-glass')
+            E{1} = csvread(['data/' name '/' object '/empty/0_right.csv']);
+            E{2} = csvread(['data/' name '/' object '/empty/1_right.csv']);
+            E{3} = csvread(['data/' name '/' object '/empty/2_right.csv']);
+            E{4} = csvread(['data/' name '/' object '/empty/3_right.csv']);
+            E{5} = csvread(['data/' name '/' object '/empty/4_right.csv']);
+            E{6} = csvread(['data/' name '/' object '/empty/5_right.csv']);
+            %
+            F{1} = csvread(['data/' name '/' object '/full/0_right.csv']);
+            F{2} = csvread(['data/' name '/' object '/full/1_right.csv']);
+            F{3} = csvread(['data/' name '/' object '/full/2_right.csv']);
+            F{4} = csvread(['data/' name '/' object '/full/3_right.csv']);
         end
-
-    % reading data of Leo plastic cup
-    elseif strcmp(name,'Leo-plastic')
-        % Empty
-        E{1} = csvread('data/Leo-plastic/empty/0_right.csv');
-        E{1}(650:end,:) = 0;
-        E{2} = csvread('data/Leo-plastic/empty/1_right.csv');
-        E{3} = csvread('data/Leo-plastic/empty/2_right.csv');
-        E{3}(1100:end,:) = 0;
-        %
-        % Full
-        F{1} = csvread('data/Leo-plastic/full/1_right.csv');
-%         F{1}(1300:end,:) = 0;
-        F{2} = csvread('data/Leo-plastic/full/2_right.csv');
-%         F{3} = csvread('data/Leo-plastic/full/3_right.csv');
-%         F{3}(2500:end,:) = 0;
          
     % reading data of Bernardo
     elseif strcmp(name,'Bernardo')
