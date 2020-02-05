8ca23c2a1375050d878c07610f255d8688c546d8
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-02-05 17:38:06 +0100

    compute F-measure

--- fullvsNOTfull/scriptAllDataBelief.m
+++ fullvsNOTfull/scriptAllDataBelief.m
@@ -8,27 +8,27 @@ addpath('../../Khansari/SEDS/SEDS_lib')
 addpath('../../Khansari/SEDS/GMR_lib')
 
 %% trained data
-Eall = [];
-Fall = [];
+Etrain = [];
+Ftrain = [];
 % Which Person to choose (Salman, Leo, Bernardo)
 [E, F] = read('Kunpeng', 'plastic-cup');
-Eall = [Eall, E];
-Fall = [Fall, F];
+Etrain = [Etrain, E];
+Ftrain = [Ftrain, F];
 [E, F] = read('Kunpeng', 'red-cup');
-Eall = [Eall, E];
-Fall = [Fall, F];
+Etrain = [Etrain, E];
+Ftrain = [Ftrain, F];
 [E, F] = read('Leo', 'red-cup');
-Eall = [Eall, E];
-Fall = [Fall, F];
+Etrain = [Etrain, E];
+Ftrain = [Ftrain, F];
 [E, F] = read('Leo', 'champagne');
-Eall = [Eall, E];
-Fall = [Fall, F];
+Etrain = [Etrain, E];
+Ftrain = [Ftrain, F];
 [E, F] = read('Leo', 'wine-glass');
-Eall = [Eall, E];
-Fall = [Fall, F];
+Etrain = [Etrain, E];
+Ftrain = [Ftrain, F];
 [E, F] = read('Athanasios', 'champagne');
-Eall = [Eall, E];
-Fall = [Fall, F];
+Etrain = [Etrain, E];
+Ftrain = [Ftrain, F];
 
 %% Load DS parameters
 
@@ -54,15 +54,50 @@ Sigma{1} = SigmaE;
 Sigma{2} = SigmaF;
 
 %% Classify train data
-classE = fun_beliefDSnorm(Eall, Priors, Mu, Sigma);
-classF = fun_beliefDSnorm(Fall, Priors, Mu, Sigma);
+[classEtrain, outEtrain] = fun_beliefDSnorm(Etrain, Priors, Mu, Sigma);
+[classFtrain, outFtrain] = fun_beliefDSnorm(Ftrain, Priors, Mu, Sigma);
 
 % Output Confusion Matrix
 
-trainETruePos = classE(1)
-trainEFalsePos = classE(2)
-trainFTrueNeg = classF(1)
-trainFFalseNeg = classF(2)
+trainETruePos = classEtrain(1)
+trainEFalsePos = classEtrain(2)
+trainFTrueNeg = classFtrain(2)
+trainFFalseNeg = classFtrain(1)
 
+%% test data
+Etest = [];
+Ftest = [];
+% Which Person to choose (Salman, Leo, Bernardo)
+[E, F] = read('Kunpeng', 'wine-glass');
+Etest = [Etest, E];
+Ftest = [Ftest, F];
+[E, F] = read('Leo', 'plastic-cup');
+Etest = [Etest, E];
+Ftest = [Ftest, F];
+[E, F] = read('Athanasios', 'red-cup');
+Etest = [Etest, E];
+Ftest = [Ftest, F];
 
+%% Classify train data
+[classEtest, outEtest] = fun_beliefDSnorm(Etest, Priors, Mu, Sigma);
+[classFtest, outFtest] = fun_beliefDSnorm(Ftest, Priors, Mu, Sigma);
 
+% Output Confusion Matrix
+
+testETruePos = classEtest(1)
+testEFalsePos = classEtest(2)
+testFTrueNeg = classFtest(2)
+testFFalseNeg = classFtest(1)
+
+%% F measure
+
+% train
+PreciTrain = trainETruePos/(trainETruePos+trainEFalsePos);
+RecallTrain = trainETruePos/(trainETruePos+trainFFalseNeg);
+
+% test
+PreciTest = testETruePos/(testETruePos+testEFalsePos);
+RecallTest = testETruePos/(testETruePos+testFFalseNeg);
+
+F1_train = 2*(PreciTrain*RecallTrain)/(PreciTrain+RecallTrain)
+F1_test  = 2*(PreciTest*RecallTest)/(PreciTest+RecallTest)
