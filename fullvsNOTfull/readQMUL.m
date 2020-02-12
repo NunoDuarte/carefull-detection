bff299ce74b21df793206544490afa168e41e408
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-02-12 10:29:40 +0100

    reading data from QMUL

--- fullvsNOTfull/readQMUL.m
+++ fullvsNOTfull/readQMUL.m
@@ -1,3 +1,22 @@
 files = dir('/home/nuno/Documents/MATLAB/PhD/armMotionDS/fullvsNOTfull/data/QMUL/*.csv');
 fullpaths = fullfile({files.folder}, {files.name});
 
+% Empty Cups
+fu0 = strfind(fullpaths, 'fu0');
+index = find(~cellfun(@isempty,fu0));
+
+for i=1:length(index)
+
+    E{i} = csvread(fullpaths{index(i)});
+    
+end
+
+% Full Cups
+fu2 = strfind(fullpaths, 'fu2');
+index = find(~cellfun(@isempty,fu2));
+
+for i=1:length(index)
+
+    F{i} = csvread(fullpaths{index(i)});
+    
+end    
