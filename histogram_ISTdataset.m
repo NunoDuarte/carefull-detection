f46f3f693a1ca9b91ee02c03b958de841fbfaef9
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-07-02 12:27:22 +0200

    finding a way to visualize the data

--- histogram_ISTdataset.m
+++ histogram_ISTdataset.m
@@ -3,10 +3,15 @@ A = Empty1;
 
 length(A)
 
-B = [];
+%   0   0.5   1   2   2.5   3   4 
+L = 0:0.001:1;
+B = zeros(7, length(L));
 
 for i=1:length(A/2)
    
+    numR = round(num, 3);
+    
+    ind = find(L == numR);
     
     
 end
