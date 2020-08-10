e12a3653e461020296af7c7d372c4299d5bc7e35
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-08-10 15:11:30 +0200

    fix bug; generate trajectory for limit cycle with matrix transformation

--- test_limitcycle.m
+++ test_limitcycle.m
@@ -56,13 +56,13 @@ figure(1)
 for i=1:1000
     hold on;
     
-    plot(ytest,xtest, '.r');    
+    plot(xtest,ytest, '.r');    
     
     r = sqrt(xtest.^2 + ytest.^2);
     phi = atan2(ytest,xtest);
 
     r_dot = -1*alpha*(r-r0);
-    phi_dot = pi/2; % rads per sec
+    phi_dot = -pi/2; % rads per sec
     
     % Limit Cycle Dynamical System in Polar Coordinates
     xd_hat =  r_dot.*cos(phi) - r.*phi_dot.*sin(phi);
@@ -70,6 +70,49 @@ for i=1:1000
 
     xd = xd_hat;
     yd = yd_hat;
+    
+    xtest = xtest + xd*0.01;
+    ytest = ytest + yd*0.01;
+    
+
+end
+
+%% Limit Cycle - Polar Coordinates with Transformation Matrix
+% define variables
+alpha = 10;
+r0 = 1;        % radius = sqrt(r0)
+theta = 1.4;      % angle rotation (radians)
+a = 1;         % scaling coefficients
+b = 7;
+x1 = 0.5;        % translation coefficients
+x2 = 0;      % radius = sqrt(r0)
+
+xtest = 1;
+ytest = 1;
+
+figure(1)
+for i=1:1000
+    hold on;
+    
+    plot(xtest,ytest, '.r');    
+
+    % diffeomorphism
+    x_hat = a.*cos(theta).*(xtest - x1) + a.*sin(theta).*(ytest - x2); 
+    y_hat = -b.*sin(theta).*(xtest - x1) + b.*cos(theta).*(ytest - x2);
+
+    r = sqrt(x_hat.^2 + y_hat.^2);
+    phi = atan2(y_hat,x_hat);
+
+    r_dot = -1*alpha*(r-r0);
+    phi_dot = -pi/2; % rads per sec
+
+    % Limit Cycle Dynamical System in Polar Coordinates
+    xd_hat =  r_dot.*cos(phi) - r.*phi_dot.*sin(phi);
+    yd_hat =  r_dot.*sin(phi) + r.*phi_dot.*cos(phi);
+
+    % Dynamical System diffeomorphism (transformation matrix)
+    xd = cos(theta).*a^(-1).*xd_hat - sin(theta).*b^(-1).*yd_hat;
+    yd = sin(theta).*a^(-1).*xd_hat + cos(theta).*b^(-1).*yd_hat;
     
     xtest = xtest + xd*0.01;
     ytest = ytest + yd*0.01;
