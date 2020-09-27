36ed1568fc2a688aead13caffba58dcdcb39a657
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2020-09-27 15:44:44 +0200

    fix bug; ellipse fitting works in matlab

--- eigen_ellipsefitting.m
+++ eigen_ellipsefitting.m
@@ -41,14 +41,43 @@ yc = K(2);
 ct = cos(theta);
 st = sin(theta);
 
-l1 = a(1)*ct*ct + a(2)*st*st + a(3)*st*ct;
-l2 = a(1)*st*st + a(2)*ct*ct - a(3)*st*ct;
+l1 = a(1)*ct*ct + a(3)*st*st + a(2)*st*ct;
+l2 = a(1)*st*st + a(3)*ct*ct - a(2)*st*ct;
 
 % scale factor
 mu = 1/(K'*A33*K - a(6));
 
 r1 = 1/(sqrt(mu*l1));
 r2 = 1/(sqrt(mu*l2));
+
+
+%% Fit ellipse to data points
+
+N = 100;
+dx = 2*pi/N;
+
+% rotation matrix
+R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+
+for i = 1:N
+   
+    t = i*dx;
+    xsim = r1*cos(t);
+    ysim = r2*sin(t);
+    rotX = R*[xsim ysim]';
+    X(i) = xc + rotX(1);
+    Y(i) = yc + rotX(2);
+    
+end
+
+figure(1)
+hold off
+plot(x,y,'ro')
+hold on
+plot(X,Y,'.b')
+
+
+
 
 
 
