e35c05c7064601635164c5d447a76b362c1c993b
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2021-02-07 16:22:09 +0000

    update paths

--- start.m
+++ start.m
@@ -5,21 +5,40 @@
 clear
 clc
 
-addpath('../SEDS')
 addpath('data')
-addpath('DS')
-addpath('beliefDS')
-addpath('../../Khansari/SEDS/SEDS_lib')
-addpath('../../Khansari/SEDS/GMR_lib')
+addpath('ds') 
+addpath('processing')
+addpath('belief')
+addpath('../../software/Khansari/SEDS/SEDS_lib')
+addpath('../../software/Khansari/SEDS/GMR_lib')
 
 %% 
 
-K = [6, 7, 8];
+K = [3, 4, 5, 6, 7, 8];
+P = [0.6, 0.7, 0.8, 0.65, 0.75, 0.85]; % percentage
 
-for i=1:4
+for i=1:length(P)
     for k = 1:length(K)
 
-       scriptAllDataDS(K(k))
+       scriptAllDataDS(K(k), P(i), ' ', ' ');
        close all;
     end
 end
+
+% K = [3, 4, 5, 6, 7, 8];
+% 
+% type = {'All', 'One'};
+% 
+% Obj = {'plastic-cup', 'red-cup', 'champagne'};
+% Hum = {'Kunpeng', 'Leo', 'Athanasios', 'David'};
+% L = [Obj, Hum];
+% 
+% for t=1:2
+%     for i=1:length(L)
+%         for k = 1:length(K)
+% 
+%            scriptAllDataDS(K(k), type{t}, L{i})
+%            close all;
+%         end
+%     end
+% end
